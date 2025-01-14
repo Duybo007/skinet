d310cd4363438693f4a191fdfcc247c9fd18161b
Duybo007 <106200147+Duybo007@users.noreply.github.com>
2025-01-14 13:10:23 -0800

    end of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,32 +1,16 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
-  private http = inject(HttpClient);
+export class AppComponent {
   
-  baseUrl = "https://localhost:5001/api/";
-  products: Product[] = [];
-
-
-  ngOnInit(): void {
-    this.getProducts();
-  }
-
-  getProducts(){
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: (response) => this.products = response.items,
-      error: (error) => console.error(error)
-    })
-  }
+  
 }
---
+++ client/src/app/core/services/shop.service.ts
@@ -0,0 +1,59 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Product } from '../../shared/models/product';
+import { Pagination } from '../../shared/models/pagination';
+import { ShopParams } from '../../shared/models/shopParams';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  private http = inject(HttpClient);
+  
+  baseUrl = "https://localhost:5001/api/";
+
+  types: string[] = [];
+  brands: string[] = [];
+
+  getProducts(shopParams: ShopParams){
+    let params = new HttpParams()
+
+    params = params.append('pageIndex', shopParams.pageNumber)
+    params = params.append('pageSize', shopParams.pageSize)
+
+
+    if(shopParams.brands.length > 0){
+      params = params.append('brands', shopParams.brands.join(','))
+    }
+
+    if(shopParams.types.length > 0){
+      params = params.append('types', shopParams.types.join(','))
+    }
+
+    if(shopParams.sort){
+      params = params.append('sort', shopParams.sort)
+    }
+
+    if(shopParams.search){
+      params = params.append('search', shopParams.search)
+    }
+
+    return this.http.get<Pagination<Product>>(this.baseUrl + 'products', {params})
+  }
+
+  getBrands(){
+    if(this.brands.length > 0) return;
+    return this.http.get<string[]>(this.baseUrl + 'products/brands').subscribe({
+      next: (data) => this.brands = data,
+      error: (error) => console.error('Error:', error)
+    })
+  }
+  
+  getTypes(){
+    if(this.types.length > 0) return;
+    return this.http.get<string[]>(this.baseUrl + 'products/types').subscribe({
+      next: (data) => this.types = data,
+      error: (error) => console.error('Error:', error)
+    })
+  }
+}
