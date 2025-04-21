09eba0d0706e5474fb2e69127f32f5a77232ccc2
Duybo007 <106200147+Duybo007@users.noreply.github.com>
2025-04-21 14:12:18 -0700

    stripe key update

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     apiUrl: 'api/',
     hubUrl: 'hub/notifications',
-    stripePublicKey: 'pk_test_51R19lsKCbR4zX7M7GlY9nP8nvrjlDvuucyQ78Y1KX61RMhXqs00w0n9MdLdYjuAn4BYzEMtSBZvVy88uEFn9hvOV00Z43Y4Gbq'
+    stripePublicKey: 'pk_test_51Qu0krPGv4AVinOzQT6vfauW9C6AVRInifrB9J4hlnzOTgjNs3tbnjkC35OOgr2u7VSjnIZkUW1i4Oq7kgBNVJ0E005AKpIw8J'
 };
 // "Redis": "evolved-firefly-57926.upstash.io:6379,password=AeJGAAIjcDE0MWU4N2I1ZjYyMmE0ZmUyOTc3ZDE0MzFhYzI5NTQ0NnAxMA,ssl=true,abortConnection=False"
