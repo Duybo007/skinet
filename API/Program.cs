af522acfd2c4a22e3e11408a2a0688d0af9af838
Duybo007 <106200147+Duybo007@users.noreply.github.com>
2025-04-21 10:23:27 -0700

    troubleshoot redis

--- API/Program.cs
+++ API/Program.cs
@@ -28,6 +28,11 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
     var connString = builder.Configuration.GetConnectionString("Redis") 
         ?? throw new Exception("Cannot get redis connection string");
     var configuration = ConfigurationOptions.Parse(connString, true);
+    configuration.Ssl = true;
+    configuration.AbortOnConnectFail = false;
+    configuration.ConnectTimeout = 10000; // 10 seconds
+    configuration.SyncTimeout = 10000; // 10 seconds
+    configuration.ConnectRetry = 3; // Retry 3 times
     return ConnectionMultiplexer.Connect(configuration);
 });
 builder.Services.AddSingleton<ICartService, CartService>();
